0fd5d19e431cfc6c447de72ada9a62d1653b4c07
Alex Wulf <alexaethelwulf@gmail.com>
2023-06-17 22:49:56 -0500

    Created User model and connected Google Next-Auth

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,19 +7,19 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 import { set } from 'mongoose'
 
 const Nav = () => {
-  const isUserLoggedIn = true
+  const { data : session } = useSession()
   
   const [ providers, setProviders ] = useState(null)
   const [toggleDropdown, setToggleDropdown] = useState(false)
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders()
 
       setProviders(response)
     }
 
-    setProviders()
+    setUpProviders()
   }, [])
 
   return (
@@ -37,7 +37,7 @@ const Nav = () => {
       </Link>
 
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -53,7 +53,7 @@ const Nav = () => {
 
             <Link href='/profile'>
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -80,10 +80,10 @@ const Nav = () => {
       </div>
 
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
